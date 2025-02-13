0b6ebeb34c8e5858dc5fd6d8d785c2a103673dff
hexachordanu <theanuragsrivastava@gmail.com>
2025-02-13 19:42:50 +0000

    Update simple-test-matrix.js

--- .github/scripts/simple-test-matrix.js
+++ .github/scripts/simple-test-matrix.js
@@ -1,13 +1,1 @@
-module.exports = ({ context }) => {
-  const matrix = {
-    include: [
-      { 'test-type': 'unit' },
-      { 'test-type': 'integration' }
-    ]
-  };
-  
-  // You can add logic here to modify the matrix based on the PR content
-  // For example, checking labels or changed files
-
-  return matrix;
-};
+console.log("Hextest")
